08cc522e9923c5dd3ad05838ee246604ccd9a347
Elomi Gire <elomi.gire.78@gmail.com>
2024-10-05 08:42:49 +0900

    tambah coding dalam file

--- index.php
+++ index.php
@@ -1,43 +1,64 @@
 <?php
 
-class Mobil{
+class Mobil {
 
-    //property
-    public $merk;
-    public $model;
-    public $tahun;
+    // Property
+    public $merk; 
+    public $model; 
+    public $tahun; 
     public $warna;
 
-    //method
+    // Method
     public function start()
     {
-        echo "mobil dinyalakan";
+        echo 'Mobil dinyalakan';
     }
 
-    public function cek($nama)
+    public function cek($nama, $ket)
     {
-        echo "merek:".$this->merk. '<br>';
-        echo "model:".$this->model. '<br>';
-        echo "tahun:".$this->tahun. '<br>';
-        echo "warna:".$this->warna. '<br>';
-        echo "keterangan:".$nama. '<br>';
-        
-
-      
+        echo 'Merek :' . $this->merk.'<br>';
+        echo 'Model :' . $this->model.'<br>';
+        echo 'Tahun :' . $this->tahun.'<br>';
+        echo 'Warna :' . $this->warna.'<br>';
+        echo 'Pemilik :' . $nama.'<br>';
+        echo 'Keterangan :' . $ket.'<br>';
+        echo '<br>';
 
     }
+
+    public function stop()
+    {
+        echo 'Mobil dimatikan';
+    }
+
 
 }
 
-//objek
+ class Kendaraan extends Mobil  {
+    public $jenis;
 
-$mobil =new Mobil();
-$mobil->merk= "Avanza";
-$mobil->model= "Nova";
-$mobil->tahun = 2823;
-$mobil->warna= "putih";
-
-//panggil method
+    public function bergerak()
+    {
+        echo 'Kendaraan sedang bergerak';
+    }
+ }
 
 
-$mobil->cek("Elomi");
+
+// Objek 
+$mobil = new Mobil();
+$kendaraan = new Kendaraan();
+$mobil->merk = 'Avanza';
+$mobil->model = 'Nova';
+$mobil->tahun = 2023;
+$mobil->warna = 'Putih';
+
+// Panggil Method
+$ket =' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur ducimus alias ab quia sunt reprehenderit consequuntur possimus inventore tempore deserunt.';
+
+$mobil->cek('Samuel',$ket);
+$kendaraan->merk = 'BWM';
+$kendaraan->model = 'Nova';
+$kendaraan->tahun = 2023;
+$kendaraan->warna = 'Putih';
+$kendaraan->cek('Sam','');
